76c641e2e4eb3167cbc76cc84c2a5f77b93bdfc2
valdoang <valdo.20010@mhs.unesa.ac.id>
2023-06-08 21:26:00 +0700

    Add Item RecyclerView and Intent Gallery

--- app/src/main/java/com/dicoding/illustrateme/view/upload/UploadActivity.kt
+++ app/src/main/java/com/dicoding/illustrateme/view/upload/UploadActivity.kt
@@ -1,15 +1,47 @@
 package com.dicoding.illustrateme.view.upload
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import com.dicoding.illustrateme.R
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.illustrateme.databinding.ActivityUploadBinding
+import com.dicoding.illustrateme.utils.uriToFile
+import java.io.File
 
 class UploadActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUploadBinding
+    private var getFile: File? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.galleryButton.setOnClickListener { startGallery() }
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@UploadActivity)
+                getFile = myFile
+                binding.previewImageView.setImageURI(uri)
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
